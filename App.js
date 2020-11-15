7f4cb005594d4a6fbbbec30b57db556da9a95db0
Bruno Rodrigues <bytebruno@gmail.com>
2020-11-15 17:47:56 -0300

    feat: Added screens boilerplate and imported in App.js
    Created the boilerplate of all the screens that I will need to use on this app

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
