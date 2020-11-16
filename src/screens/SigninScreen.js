63546ac0424d57ff05f4bad8a95c8a47994f5744
Bruno Rodrigues <bytebruno@gmail.com>
2020-11-16 18:19:49 -0300

    feat: Installed react-native-elements-library
    Installed react-native-elements library to save time on component development
    and created a helper component to add some margin to its children components

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, Text } from 'react-native';
 
+
 const SigninScreen = () => {
     return <Text style={{fontSize:48}}>SigninScreen</Text>;
 }
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import {View, StyleSheet, Text, Button } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
-const SignupScreen = ({navigation}) => {
-    return <>
-        <Text style={{fontSize:48}}>SignupScreen</Text>
-        <Button title="Go to Signin" onPress={()=> navigation.navigate('Signin')} />
-        <Button title="Go to main flow" onPress={()=> navigation.navigate('mainFlow')} />
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </>
-}
+  );
+};
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
 
 export default SignupScreen;
